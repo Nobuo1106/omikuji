ae6feae0b8540cf522a811f91a032c3a09b8294f
Iga <gonnbe1106@yahoo.co.jp>
2019-04-06 21:11:31 +0900

    completed

--- js/main.js
+++ js/main.js
@@ -2,28 +2,33 @@
 'use strict';
 
 {
-// 定数btnにhtmlIdのbtnを代入する。
+  // 定数btnにhtmlIdのbtnを代入する。
   const btn = document.getElementById('btn');
-//定数nにランダムな乱数0-1を代入する。
-    const n = Math.random();
-// もし0.05(0.05/1 = 5%)より小さければ大吉
-    if (n < 0.05) {
-      btn.textContent = '大吉';
-// もし0.2((0.2-0.05)/1 = 15%)より小さければ小吉
-    } else if (n < 0.2) {
-      btn.textContent = '中吉';
-// それ以外は凶
-    } else {
-      btn.textContent = '凶';
-    }
-  });
-// 定数btnのマウスがクリックダウンされた時のイベント処理
-  btn.addEventListener('mousedown', () =>{
+
+  btn.addEventListener('click', () =>{
+
+
+  //定数nにランダムな乱数0-1を代入する。
+  const n = Math.random();
+  // もし0.05(0.05/1 = 5%)より小さければ大吉
+  if (n < 0.05) {
+    btn.textContent = '大吉';
+  // もし0.2((0.2-0.05)/1 = 15%)より小さければ小吉
+  } else if (n < 0.2) {
+    btn.textContent = '中吉';
+  // それ以外は凶
+  } else {
+    btn.textContent = '凶';
+  }
+
+  // 定数btnのマウスがクリックダウンされた時のイベント処理
+  btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
   });
-// 定数btnのマウスがクリックアップされた時のイベント処理
-  btn.addEventListener('mouseup', () =>{
-// 定数btnからpressedクラスを取り除く
+  // 定数btnのマウスがクリックアップされた時のイベント処理
+  btn.addEventListener('mouseup', () => {
+    // 定数btnからpressedクラスを取り除く
     btn.classList.remove('pressed');
   });
+  })
 }
